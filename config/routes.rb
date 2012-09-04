2755a8118f8f2e493d8fe3ac9d27986ba5beffbc
nao <example@example.com>
2012-09-04 23:18:17 +0900

    fixed filter link and routes bug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ExceptionLogger::Engine.routes.draw do
   root :to => "logged_exceptions#index"
-  match "/query" => "logged_exceptions#query", :as => :query, :via => :post
+  match "/query" => "logged_exceptions#query", :as => :query, :via => :get
   match "/destroy_all" => "logged_exceptions#destroy_all", :as => :destroy_all, :via => :post
   match "/feed" => "logged_exceptions#feed", :as => :feed, :via => :get
   match "/:id" => "logged_exceptions#show", :via => :get
