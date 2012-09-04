ae3f238a13a0413ae5806da743b1d8ae48376b2b
nao <example@example.com>
2012-09-04 21:12:34 +0900

    fixed undefined method `per' exception. (kaminari).

--- app/controllers/exception_logger/logged_exceptions_controller.rb
+++ app/controllers/exception_logger/logged_exceptions_controller.rb
@@ -31,28 +31,26 @@ module ExceptionLogger
     end
 
     def query
-      exceptions = LoggedException.sorted
+      @exceptions = LoggedException.page(params[:page]).per(25).sorted
       unless params[:id].blank?
-        exceptions = exceptions.where(:id => params[:id])
+        @exceptions = @exceptions.where(:id => params[:id])
       end
       unless params[:query].blank?
-        exceptions = exceptions.message_like(params[:query])
+        @exceptions = @exceptions.message_like(params[:query])
       end
       unless params[:date_ranges_filter].blank?
-        exceptions = exceptions.days_old(params[:date_ranges_filter])
+        @exceptions = exceptions.days_old(params[:date_ranges_filter])
       end
       unless params[:exception_names_filter].blank?
-        exceptions = exceptions.by_exception_class(params[:exception_names_filter])
+        @exceptions = @exceptions.by_exception_class(params[:exception_names_filter])
       end
       unless params[:controller_actions_filter].blank?
         c_a_params = params[:controller_actions_filter].split('/')
         controller_filter = c_a_params.first.underscore
         action_filter = c_a_params.last.downcase
-        exceptions = exceptions.by_controller(controller_filter)
-        exceptions = exceptions.by_action(action_filter)
+        @exceptions = @exceptions.by_controller(controller_filter)
+        @exceptions = @exceptions.by_action(action_filter)
       end
-
-      @exceptions = exceptions.page(params[:page]).per(25)
 
       respond_to do |format|
         format.html { redirect_to :action => 'index' unless action_name == 'index' }
